429f76ef4265d0dc0b415cb8ba227497e1768934
Kannan <75429470+kannanoo7@users.noreply.github.com>
2021-12-24 20:42:17 +0530

    create method.java
    This program show the methods calling

---
+++ methods.java
@@ -0,0 +1,25 @@
+
+
+
+
+class Employee{
+	public void Details(){
+		System.out.println("Name"+"\t Year of joining"+"\tAddress");
+		System.out.println("Robert"+"\t\t1994"+"\t\t64c-WallsStreet");
+		System.out.println("Sam"+"\t\t2000"+"\t\t68D-WallsStreet");
+		System.out.println("john"+"\t\t1999"+"\t\t26B-WallsStreet");
+		
+	}
+	
+	
+}
+public class Details {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Employee t1=new Employee();
+	t1.Details();
+		
+	}
+
+}
